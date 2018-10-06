d4659163a54c58db029a5be6ebdb7af6d4ded75d
honglux <xuhlusa@gmail.com>
2018-10-06 18:53:38 -0400

    Missile Defence Start

---
+++ VOR_head/Assets/Scripts/MissileDefence/MD_GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MD_GameController : MonoBehaviour {
+
+    public float InstRandomRange = 9.0f;
+    public float InstY = 4.0f;
+    public float InstZ = 10.0f;
+
+    public GameObject MissilePrefab;
+    //private Missile Missile_Script;
+
+    public GameObject City1_GO;
+    public GameObject City2_GO;
+    public GameObject City3_GO;
+
+    private List<GameObject> cities;
+
+    // Use this for initialization
+    void Start () {
+
+        this.cities = new List<GameObject>();
+
+        cities.Add(City1_GO);
+        cities.Add(City2_GO);
+        cities.Add(City3_GO);
+
+        //Missile_Script.set_target(City1_GO.transform);
+        //Missile_Script.start_move();
+
+        //Missile_Script.face(City1_GO.transform.position);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ToInstantiatetMissile()
+    {
+        GameObject Missile =
+                (GameObject)Instantiate(MissilePrefab, RandomPosGenerate(), new Quaternion());
+        Missile Missile_Script = Missile.GetComponent<Missile>();
+        int city_number = Random.Range(0, 3);
+        Debug.Log("city_number " + city_number);
+        Missile_Script.set_target(cities[city_number].transform);
+        //Missile_Script.start_move();
+        Missile_Script.start_flag = true;
+    }
+
+    private Vector3 RandomPosGenerate()
+    {
+        return new Vector3(Random.Range(-InstRandomRange, InstRandomRange),
+                            InstY, InstZ);
+    }
+}
---
+++ VOR_head/Assets/Scripts/MissileDefence/Missile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+
+    public float Speed = 0.5f;
+
+    public bool start_flag;
+    private Vector3 Target_pos;
+
+
+    // Use this for initialization
+    void Start () {
+        this.Target_pos = new Vector3();
+        this.start_flag = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Debug.Log("start_flag " + start_flag);
+        if (start_flag)
+        {
+            Debug.Log("Moving Target_pos "+ Target_pos);
+            transform.Translate(Target_pos * Time.deltaTime * Speed, Space.World);
+        }
+    }
+
+    public void set_target(Transform tar_transform)
+    {
+        Debug.Log("tar_transform.position " + tar_transform.position);
+        Debug.Log("transform.position " + transform.position);
+
+        Target_pos = tar_transform.position - transform.position;
+        Debug.Log("Target_pos " + Target_pos);
+    }
+
+    public void start_move()
+    {
+        face(Target_pos);
+        Debug.Log("Target_pos2 " + Target_pos);
+        start_flag = true;
+        Debug.Log("start_flag2 " + start_flag);
+
+    }
+
+    public void face(Vector3 pos)
+    {
+        transform.up = -pos;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("11111");
+        if(other.transform.gameObject.tag == "City")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
