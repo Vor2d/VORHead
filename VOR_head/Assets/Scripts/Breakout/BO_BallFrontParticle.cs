22e8df7271c200f624258c43d72e045b6b5c58bd
honglux <xuhlusa@gmail.com>
2018-12-03 17:08:47 -0500

    BO front particle system

---
+++ VOR_head/Assets/Scripts/Breakout/BO_BallFrontParticle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BO_BallFrontParticle : MonoBehaviour {
+
+    private bool particle_on;
+
+	// Use this for initialization
+	void Start () {
+        this.particle_on = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if( other.name == "PadColliders" &&  !particle_on)
+        {
+            GetComponent<ParticleSystem>().Play();
+            //particle_on = true;
+            Debug.Log("OnTriggerEnter ball");
+        }
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.name == "PadColliders" && particle_on)
+    //    {
+    //        GetComponent<ParticleSystem>().Stop();
+    //        particle_on = false;
+    //    }
+
+    //}
+}
