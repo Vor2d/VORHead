14f3b14267918807280fc8efa07e513e126e4c77
honglux <xuhlusa@gmail.com>
2021-01-01 22:36:35 -0500

    RRTest

---
+++ RefreshRateTest/Assets/Scripts/RRTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RRTest : MonoBehaviour
+{
+    [SerializeField] private TextMesh tM1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        test1();
+    }
+
+    private void test1()
+    {
+        tM1.text = (1.0f / Time.deltaTime).ToString("F4");
+    }
+}
